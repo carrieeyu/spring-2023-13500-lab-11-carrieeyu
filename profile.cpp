41f999b4357f5d9200c8f5b5a8df77f0d5432105
Carrie <carrieyu@DESKTOP-N1A0LCS.localdomain>
2023-04-25 20:14:31 -0400

    completed task A

--- profile.cpp
+++ profile.cpp
@@ -1,1 +1,27 @@
 //task A; "social" from genady's lab 11
+
+#include <iostream>
+#include "profile.h"
+
+std:: string Profile:: getUsername(){ //"Profile:: " refers to the private class vars
+  return username; //returns username
+}
+
+std:: string Profile:: getFullName(){
+  return displayname; //returns name in the format: "displayname (@username)"
+}
+
+void Profile:: setDisplayName(std:: string dspn){ //change display name
+  displayname = dspn; //set current display name to whatever is asked for in main
+}
+
+//constructor values for access to the private member vars above
+Profile:: Profile(std:: string usrn, std:: string dspn){
+  username = usrn;
+  displayname = dspn;
+}
+Profile:: Profile(){
+  username = "";
+  displayname = "";
+}
+
--- profile.h
+++ profile.h
@@ -1,1 +1,25 @@
-.
+#pragma once
+#include <iostream>
+
+class Profile {
+
+//can be used only internally by the class itself; cannot be directly modified or accessed from outside
+private:
+    std:: string username;
+    std:: string displayname;
+
+//can be called by other classes
+public:
+    // Profile constructor for a user (initializing 
+    // private variables username=usrn, displayname=dspn)
+    Profile(std:: string usrn, std:: string dspn);
+
+    // Default Profile constructor (username="", displayname="")
+    Profile();
+
+    //functions
+    std:: string getUsername(); //Return username
+    std:: string getFullName(); // Return name in the format: "displayname (@username)"
+    void setDisplayName(std:: string dspn); //Change display name
+};
+
